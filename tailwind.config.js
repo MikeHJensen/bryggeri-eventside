c8d6b67a13b3b1ee8838498a8e8e8e0cc1c1e434
Philip Olesen <nbphilip@gmail.com>
2024-06-12 15:56:02 +0200

    nav and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      'green': '#397c67',
+      'lightgreen': '#50b697',
+      'navhover': '#132b1d',
+    },
     extend: {},
   },
   plugins: [],
