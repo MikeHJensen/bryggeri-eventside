12d940c2c66403532a0b5f345fff68ad23947e15
Philip Olesen <nbphilip@gmail.com>
2024-06-13 17:38:11 +0200

    finito??????

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     colors: {
-      'green': '#0f6046',
+      'moellegreen': '#0f6046',
       'lightgreen': '#50b697',
       'navhover': '#ebc83b',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      red: colors.red,
+      green: colors.green,
     },
     extend: {
       fontFamily: {
+        sans: ['tahoma', 'sans-serif'],
         kellyslab: ['Kelly Slab', 'serif'],
       }
     },
